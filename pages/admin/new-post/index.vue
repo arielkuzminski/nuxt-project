18f4928bc6bad70a96f062829bf82803d941147b
Ariel Kuźmiński <a.kuzminski@nowaera.pl>
2018-11-27 16:47:30 +0100

    Save data in db

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="admin-new-post-page">
     <section class="new-post-form">
-      <AdminPostForm />
+      <AdminPostForm @submit="onSubmitted" />
     </section>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import AdminPostForm from "@/components/Admin/AdminPostForm";
 
 export default {
   layout: 'admin',
   components: {
     AdminPostForm
+  },
+  methods: {
+    onSubmitted(postData) {
+      console.log(postData);
+      axios.post('https://nuxt-blog-c487f.firebaseio.com/posts.json', postData)
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+
+    }
   }
 };
 </script>
