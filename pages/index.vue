4348efb42714ef3948db983acb6f2d0396617d47
Ariel Kuźmiński <a.kuzminski@nowaera.pl>
2018-11-27 15:03:14 +0100

    Add Vuex

--- pages/index.vue
+++ pages/index.vue
@@ -14,43 +14,11 @@ export default {
   components: {
     PostList
   },
-  asyncData(context) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          loadedPosts: [
-            {
-              id: "1",
-              title: "First Post",
-              previewText: "This is our first post!",
-              thumbnail:
-                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-            },
-            {
-              id: "2",
-              title: "Second Post",
-              previewText: "This is our second post!",
-              thumbnail:
-                "https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-            }
-          ]
-        });
-      }, 1000);
-    })
-    .then(data => {
-      return data
-    })
-    .catch(e => {
-      context.error(new Error());
-    });
-    console.log(context);
-    console.log('Async data executed');
+  computed: {
+    loadedPosts() {
+      return this.$store.getters.loadedPosts;
+    }
   }
-  // data() {
-  //   return {
-  //     loadedPosts: []
-  //   };
-  // },
 };
 </script>
 
