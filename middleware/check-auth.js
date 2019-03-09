051be431687989838f7e7a8bfb1c11083c34cf7a
Ariel <ariel.kuzminski@gmail.com>
2019-03-09 15:50:41 +0100

    Add cookies

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -1,6 +1,8 @@
 export default function(context) {
   console.log('Middleware check-auth');
   if (process.client) {
-    context.store.dispatch('initAuth');
+    context.store.dispatch('initAuth', null);
+  } else {
+    context.store.dispatch('initAuth', context.req);
   }
 }
